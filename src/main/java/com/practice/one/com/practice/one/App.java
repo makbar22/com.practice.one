4cb9a9222041de4dc9e8df64f2df2ee6b80c530f
Murtaza Akbar <makbar22>
2020-07-03 22:03:53 -0400

    first practice

---
+++ src/main/java/com/practice/one/com/practice/one/App.java
@@ -0,0 +1,13 @@
+package com.practice.one.com.practice.one;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
